c0d0dd525ee0da433df270e29f1fd0c5d5c7986d
Shovit Nepal <65544518+sobit-nep@users.noreply.github.com>
2023-08-14 23:20:22 +0545

    Delete Desktop/hyperce directory

--- Desktop/hyperce/backend/todo/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Task  # Importing the model
-
-admin.site.register(Task)  # Registering the model with the admin site
--- Desktop/hyperce/frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-// src/api.js
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'http://localhost:8000',  // Replace with your Django backend URL
-});
-
-export default instance;
